3907cb5ddb5e40bb7d853e4983c871ec02ad1bb5
Evan <81714339+skysarwer@users.noreply.github.com>
2022-10-21 03:14:19 -0400

    Homepage hero complete

--- inc/homepage-hero.php
+++ inc/homepage-hero.php
@@ -5,12 +5,17 @@ function evn_do_homepage_hero() {
 
     <div class="homepage-hero">
         <div class="content-wrap homepage-content">
+
             <div>
-                <h1><?php _e('Websites built on <br/> strong foundations', 'evn');?></h1>
-                <p><?php _e('Hi, my name is Evan. I build systems that are tailor fit to the needs of the individuals that I work with.', 'evn');?></p>
-                <p><?php _e('I love solving complex problems in pursuit of ambitious designs.  I strive to provide work that is scalable, practical and which endures the test of time.', 'evn');?></p>
-                <button class="cta"><?php _e('Work with me', 'evn');?></button>
+                <h1><?php _e('Hi, my name is Evan', 'evn');?></h1>
+                <p><?php _e('I build professional websites, tailor fit to the needs of both the people that I work with and their end users.', 'evn');?></p>
+                <p><?php _e('I love solving complex problems in pursuit of ambitious designs. I strive to provide work that is scalable, practical and user friendly.', 'evn');?></p>
+                <button class="cta" type="button" data-a11y-dialog-show="contact_cta"><?php _e('Work with me', 'evn');?> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="transparent" stroke="currentColor"><path stroke="none" d="M0 0h24v24H0z"></path><polyline points="9 6 15 12 9 18"></polyline></svg></button>
+                <a class="button" href="#primary"><?php _e('Learn more', 'evn');?> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="transparent" stroke="currentColor"><path stroke="none" d="M0 0h24v24H0z"></path><polyline points="9 6 15 12 9 18"></polyline></svg></a>
             </div>
+            <figure id="portrait">
+                <img src="<?php echo get_theme_file_uri('img/evan_b_portrait.jpg');?>"  alt="Portrait of Evan Buckiewicz" srcset="<?php echo get_theme_file_uri('img/evan_b_portrait-scaled.jpg');?>" draggable="false">
+            </figure>
         </div>
     </div>
     <?php echo ob_get_clean();
