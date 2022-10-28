2b6db6b447478fc2fbab27a26693eb5738444ed9
Evan <81714339+skysarwer@users.noreply.github.com>
2022-10-28 18:28:52 -0400

    Social media icons support

--- footer.php
+++ footer.php
@@ -27,17 +27,24 @@
 </button>
 
 </div><!-- #page -->
-	<footer id="colophon" class="site-footer">
-		<div class="footer-bottom content-wrap">
-			<div class="address">
-				<p class="is-style-font-semibold"><?php echo evn_marker_svg();?> Located in Mile End,<br/>  Montreal, QC Canada</p> 
-			</div>
-			<div class="contact">
-			<p><span class="is-style-font-bold"><?php echo evn_svg_mail();?> Email : </span><a href="mailto:contact@evan.works" title="Email contact@evan.works">contact@evan.works</a></p>
-			<p><span class="is-style-font-bold"><?php echo evn_svg_phone();?>Phone : </span><a href="tel:4388303585">+1 (438) 830-3585</span></a>
-			</div>
+
+<footer id="colophon" class="site-footer content-wrap">
+	<div class="footer-top">
+		<div class="social-media">
+			<a class="icon" href="https://linkedin.com/in/evanbuckiewicz" target="_blank" title="LinkedIN"><?php echo evn_svg_linkedin();?><span class="screen-reader-text">LinkedIN</span></a>
+			<a class="icon" href="https://github.com/skysarwer" target="_blank" title="Github"><?php echo evn_svg_github();?><span class="screen-reader-text">Github</span></a>
 		</div>
-	</footer><!-- #colophon -->
+	</div>
+	<div class="footer-bottom">
+		<div class="address">
+			<p class="is-style-font-semibold"><?php echo evn_marker_svg();?> Located in Mile End,<br/>  Montreal, QC Canada</p> 
+		</div>
+		<div class="contact">
+		<p><span class="is-style-font-bold"><?php echo evn_svg_mail();?> Email : </span><a href="mailto:contact@evan.works" title="Email contact@evan.works">contact@evan.works</a></p>
+		<p><span class="is-style-font-bold"><?php echo evn_svg_phone();?>Phone : </span><a href="tel:4388303585">+1 (438) 830-3585</span></a>
+		</div>
+	</div>
+</footer><!-- #colophon -->
 
 
 <?php wp_footer(); ?>
