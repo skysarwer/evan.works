6022867c02aa5dfa9f56e2e4da6dab1cf069f08d
Evan <81714339+skysarwer@users.noreply.github.com>
2023-01-23 19:59:24 -0500

    Nav updates (pick)

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,7 @@
  * Handles toggling the navigation menu for small screens and enables TAB key
  * navigation support for dropdown menus.
  */
-( function($) {
+( function() {
 	const siteNavigation = document.getElementById( 'site-navigation' );
 
 	// Return early if the navigation doesn't exist.
@@ -58,13 +58,46 @@
 		}
 	} );
 
+	const submenus = document.querySelectorAll('.sub-menu');
+	// Get all the submenu toggle buttons within the menu.
+	const submenuToggles = menu.querySelectorAll( '.menu-item-has-children .submenu-toggle, .page_item_has_children .submenu-toggle' );
+
+	document.addEventListener('click', (event) => {
+		if(!event.target.closest('.submenu-toggle') && !event.target.closest('.sub-menu')){
+			submenus.forEach(submenu => submenu.classList.remove('toggled'));
+			submenuToggles.forEach(toggleButton => {
+				toggleButton.setAttribute('aria-expanded', 'false'); 
+				//toggleButton.innerHTML = '+'
+			});
+		}
+	});
+
+	submenus.forEach(submenu => {
+		submenu.classList.add('offscreen');
+		submenu.style.setProperty('--submenu-height', 'calc(' + submenu.scrollHeight + 'px + 2em)');
+		submenu.classList.remove('offscreen');
+	});
+
+
+	window.addEventListener('resize', () => {
+
+		submenuToggles.forEach(toggleButton => {
+			toggleButton.setAttribute('aria-expanded', 'false'); 
+			//toggleButton.innerHTML = '+'
+		});
+
+		submenus.forEach(submenu => {
+			submenu.classList.remove('toggled');
+			submenu.classList.add('offscreen');
+			submenu.style.setProperty('--submenu-height', 'calc(' + submenu.scrollHeight + 'px + 2em)');
+			submenu.classList.remove('offscreen');
+		});
+	});
+
 	// Get all the link elements within the menu.
 	const links = menu.getElementsByTagName( 'a' );
 
 	const linksWithChildren = menu.querySelectorAll( '.menu-item-has-children a, .page_item_has_children a' );
-
-	// Get all the submenu toggle buttons within the menu.
-	const submenuToggles = menu.querySelectorAll( '.menu-item-has-children .submenu-toggle, .page_item_has_children .submenu-toggle' );
 
 	// Toggle focus each time a menu link is focused or blurred.
 	for ( const link of links ) {
@@ -83,14 +116,15 @@
 
 		// Toggle the the .toggled class and the aria-expanded value each time a .submenu-toggle button is clicked.
 		toggle.addEventListener( 'click', function() {
-			$(submenu).slideToggle( );
 
 			if ( toggle.getAttribute( 'aria-expanded' ) === 'true' ) {
 				toggle.innerHTML = '+'
 				toggle.setAttribute( 'aria-expanded', 'false' );
+				submenu.classList.remove( 'toggled' );
 			} else {
 				toggle.innerHTML = '-'
 				toggle.setAttribute( 'aria-expanded', 'true' );
+				submenu.classList.add( 'toggled' );
 			}
 		} );
 	}
@@ -122,4 +156,4 @@
 			menuItem.classList.toggle( 'focus' );
 		}
 	}
-})(jQuery);
+})();
