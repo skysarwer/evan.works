bce9d659ab81f50312f34cbfbbf1c105555ddf2f
Evan <81714339+skysarwer@users.noreply.github.com>
2023-01-23 19:17:21 -0500

    Hide title functionality

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,35 @@ function evn_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'evn_pingback_header' );
+
+function evn_add_hide_title_metabox() {
+    add_meta_box(
+        'hide_title_metabox', // Metabox ID
+        'Hide Title', // Metabox title
+        'evn_render_hide_title_metabox', // Callback function to render the metabox
+        'page', // Post type
+        'side', // Context (normal, advanced, or side)
+        'default' // Priority (high, core, default, or low)
+    );
+}
+add_action( 'add_meta_boxes', 'evn_add_hide_title_metabox' );
+
+function evn_render_hide_title_metabox( $post ) {
+    $hide_title = get_post_meta( $post->ID, 'hide_title', true );
+
+    ?>
+    <label for="hide_title">
+        <input type="checkbox" name="hide_title" id="hide_title" value="1" <?php checked( $hide_title, 1 ); ?>>
+        Hide Title
+    </label>
+    <?php
+}
+
+function evn_save_hide_title_metabox( $post_id ) {
+    if ( isset( $_POST['hide_title'] ) ) {
+        update_post_meta( $post_id, 'hide_title', 1 );
+    } else {
+        update_post_meta( $post_id, 'hide_title', 0 );
+    }
+}
+add_action( 'save_post', 'evn_save_hide_title_metabox' );
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,11 +11,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
+	<?php if(get_post_meta($id, 'hide_title' , true) != 1): ?> 
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<?php evn_post_thumbnail(); ?>
 
